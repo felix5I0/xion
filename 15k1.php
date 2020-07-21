ffe82e3fe36fc51b0ce4f2a01cf45c30c59f3bf8
XionManSit <67290509+XionManSit@users.noreply.github.com>
2020-07-21 20:59:30 +0700

    Update 15k1.php

--- 15k1.php
+++ 15k1.php
@@ -9,7 +9,7 @@ $token = trim(fgets(STDIN));
         echo color("blue","Sabar ");
         sleep(20);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PESANGOFOOD0607"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PESANGOFOOD2107"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
         echo "\n".color("green","🔓▶️ Message: ".$message);
